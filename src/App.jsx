5cd4ea20d6a8f1dbb93a4442f5928734d7ae7b0e
tratchapong <tratchapong@gmail.com>
2023-06-03 14:05:20 +0700

    vite start with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-3xl font-bold underline text-lime-400 text-center bg-pink-500 p-2">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
